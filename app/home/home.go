c456b78adfc64db0488bb41354786d55659ab9c0
yaziedda <yazieddabr@gmail.com>
2016-06-14 17:15:42 +0700

    awa

--- app/home/home.go
+++ app/home/home.go
@@ -4,12 +4,7 @@ import (
 	"github.com/dizzay-web/app/common"
 	"html/template"
 	"net/http"
-	// "github.com/gorilla/mux"
 )
-
-// func RouteHome(s *mux.Router) {
-// 	s.HandleFunc("/", f)
-// }
 
 func HomeViewHandler(rw http.ResponseWriter, req *http.Request) {
 	type Page struct {
@@ -38,47 +33,11 @@ func ResumeViewHandler(w http.ResponseWriter, r *http.Request) {
 
 	p := Page{
 		PageName: "resume",
-		Title:    "Resume",
+		Title:    "Skills",
 		Content:  "Sample web with golang",
 	}
 
 	common.Templates = template.Must(template.ParseFiles("layout/home/resume.html", common.LayoutPath))
-	err := common.Templates.ExecuteTemplate(w, "base", p)
-	common.CheckError(err)
-}
-
-func ContactViewHandler(w http.ResponseWriter, r *http.Request) {
-	type Page struct {
-		PageName string
-		Title    string
-		Content  string
-	}
-
-	p := Page{
-		PageName: "contact",
-		Title:    "Contact",
-		Content:  "Sample web with golang",
-	}
-
-	common.Templates = template.Must(template.ParseFiles("layout/home/contact.html", common.LayoutPath))
-	err := common.Templates.ExecuteTemplate(w, "base", p)
-	common.CheckError(err)
-}
-
-func ExperienceViewHandler(w http.ResponseWriter, r *http.Request) {
-	type Page struct {
-		PageName string
-		Title    string
-		Content  string
-	}
-
-	p := Page{
-		PageName: "experience",
-		Title:    "Experience",
-		Content:  "Sample web with golang",
-	}
-
-	common.Templates = template.Must(template.ParseFiles("layout/home/experience.html", common.LayoutPath))
 	err := common.Templates.ExecuteTemplate(w, "base", p)
 	common.CheckError(err)
 }
